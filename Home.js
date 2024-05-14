85d8551bd28c05740cc46bba2430ddc3981bcdf8
mitpatel131999 <72972092+mitpatel131999@users.noreply.github.com>
2024-05-14 16:02:07 +1000

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome to Bike & Scooter Rentals</h2>
+            <p>Choose from a wide range of bikes and scooters for your next adventure!</p>
+        </div>
+    );
+};
+
+export default Home;
